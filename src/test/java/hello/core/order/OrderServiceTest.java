0480d6d1f60d4d4c544349c53d3dc354901527fc
otteru <otteru@naver.com>
2024-06-08 16:17:54 +0900

    다양한 의존관계 주입 방법

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,10 +1,8 @@
 package hello.core.order;
 
 import hello.core.AppConfig;
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -31,4 +29,12 @@ public class OrderServiceTest {
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
+
+//    @Test
+//    void fieldInjectionTest() {
+//        OrderServiceImpl orderService = new OrderServiceImpl();
+//
+//
+//        orderService.createOrder(1L, "itemA", 10000);
+//    }
 }
