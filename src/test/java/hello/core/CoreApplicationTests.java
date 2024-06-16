dfa519e97c3af3f8f8a42527afc89170f92999c1
otteru <otteru@naver.com>
2024-06-16 14:05:40 +0900

    자동 수동의 올바른 실무 기준

--- src/test/java/hello/core/CoreApplicationTests.java
+++ src/test/java/hello/core/CoreApplicationTests.java
@@ -1,6 +1,7 @@
 package hello.core;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
